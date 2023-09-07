d29e93b775ddbcaeed91f2453117dbdcfa8b6dab
Zen <fauz.fauz23@gmail.com>
2023-09-07 11:15:07 +0700

    add

--- src/controller/web3_eth_controller.js
+++ src/controller/web3_eth_controller.js
@@ -1,9 +1,7 @@
 const { Web3 } = require("web3");
 
 const createAddress = async (req, res, next) => {
-  const web3 = new Web3(
-    new Web3.providers.HttpProvider("https://ethereum.publicnode.com")
-  );
+  const web3 = new Web3(new Web3.providers.HttpProvider("YOUR_NODE_HTTP_URL"));
 
   let dataAccount = await web3.eth.accounts.create();
   res.status(200).json({
